7ba31c85cb17e03c0685c426f5b86c7ebb43f208
johnnycao“TestCIgit config --global user.na.r.johnnycao“ <johnnycao@chinasystems.com>
2018-09-17 14:53:39 +0800

    11

--- src/main/java/com/cs/ms/baseapp/web/MessageFilter.java
+++ src/main/java/com/cs/ms/baseapp/web/MessageFilter.java
@@ -12,36 +12,60 @@ import javax.servlet.annotation.WebFilter;
 /**
  * Servlet Filter implementation class MessageFilter
  */
-@WebFilter(urlPatterns={"/*"})
+@WebFilter(urlPatterns = { "/*" })
 public class MessageFilter implements Filter {
 
-    /**
-     * Default constructor. 
-     */
-    public MessageFilter() {
-      
-    }
+	Filter filter = null;
+
+	/**
+	 * Default constructor.
+	 */
+	public MessageFilter() {
+
+	}
 
 	/**
 	 * @see Filter#destroy()
 	 */
 	public void destroy() {
-	 
+
 	}
 
 	/**
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
- 
-		System.out.println("My Web Filter...");
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		System.out.println("Start to do Web Filter...");
+
+		if (this.filter != null)
+			this.filter.doFilter(request, response, chain);
 		chain.doFilter(request, response);
 	}
 
 	/**
 	 * @see Filter#init(FilterConfig)
 	 */
-	public void init(FilterConfig fConfig) throws ServletException { 
+	public void init(FilterConfig fConfig) throws ServletException {
+
+		filter = this.getFilter();
 	}
 
+	private Filter getFilter() {
+
+		String fn = "WEB_FILTER_COMP_NAME";
+		String name = System.getenv().get(fn);
+		if (name == null)
+			name = System.getProperty(fn);
+
+		System.out.println("WEB_FILTER_COMP_NAME is " + name);
+
+		try {
+			return (Filter) Class.forName(name).newInstance();
+		} catch (Exception e) {
+			return null;
+		}
+
+	}
 }
